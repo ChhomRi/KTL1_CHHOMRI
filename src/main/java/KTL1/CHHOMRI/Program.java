10f18b7a9debc2887acfbfb4e4a56cef07f19a40
DELL <DELL@DESKTOP-OEE6G0D>
2024-08-28 20:52:12 +0700

    Phien ban 1

---
+++ src/main/java/KTL1/CHHOMRI/Program.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package KTL1.CHHOMRI;
+
+/**
+ *
+ * @author DELL
+ */
+public class Program {
+    public static void main(String[] args) {
+         QuanLySach qls = new QuanLySach();
+        qls.menu();
+    }
+}
